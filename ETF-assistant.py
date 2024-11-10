54a1898e018c6c9766092a0428ccb5d156174365
Mason Maeder <mjmaeder@wisc.edu>
2024-11-10 00:44:00 -0600

    Add support for .pdf context

--- ETF-assistant.py
+++ ETF-assistant.py
@@ -1,55 +1,78 @@
-# pip install llama-index
-# pip install langchain
+from openai import OpenAI
 
-from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper, ServiceContext
-from langchain import OpenAI
-import sys
-import os
-from IPython.display import Markdown, display
+with open('.zshrc', 'r') as f:
+    api_key = f.read().strip()
+    print("API key loaded successfully.")
 
+client = OpenAI(api_key=api_key)
 
-def construct_index(directory_path):
-    # set maximum input size
-    max_input_size = 4096
-    # set number of output tokens
-    num_outputs = 2000
-    # set maximum chunk overlap
-    max_chunk_overlap = 20
-    # set chunk size limit
-    chunk_size_limit = 600
+assistant = client.beta.assistants.create(
+    name="Employee Trust Funds Customer Service Assistant",
+    instructions="You are an expert ETF employee. Use you knowledge base to answer questions from customers.",
+    model="gpt-4o-mini",
+    tools=[{"type": "file_search"}],
+)
 
-    # define LLM
-    llm_predictor = LLMPredictor(llm=OpenAI(
-        temperature=1, model_name="text-davinci-003", max_tokens=num_outputs))
-    prompt_helper = PromptHelper(
-        max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+vector_store = client.beta.vector_stores.create(name="ETF Documents")
 
-    documents = SimpleDirectoryReader(directory_path).load_data()
+# Ready the files for upload to OpenAI
+file_paths = ["etf/et4118.pdf", "etf/et4121.pdf", "etf/et4132.pdf"]
+file_streams = [open(path, "rb") for path in file_paths]
 
-    service_context = ServiceContext.from_defaults(
-        llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-    index = GPTSimpleVectorIndex.from_documents(
-        documents, service_context=service_context)
+# Use the upload and poll SDK helper to upload the files, add them to the vector store,
+# and poll the status of the file batch for completion.
+file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
+    vector_store_id=vector_store.id, files=file_streams
+)
 
-    index.save_to_disk('index.json')
+# You can print the status and the file counts of the batch to see the result of this operation.
+print(file_batch.status)
+print(file_batch.file_counts)
 
-    return index
+assistant = client.beta.assistants.update(
+    assistant_id=assistant.id,
+    tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
+)
 
+# Upload the user provided file to OpenAI
+message_file = client.files.create(
+    file=open("etf/et4121.pdf", "rb"), purpose="assistants"
+)
 
-def ask_ai():
-    index = GPTSimpleVectorIndex.load_from_disk('index.json')
-    while True:
-        query = input("What do you want to ask? ")
-        response = index.query(query, response_mode="compact")
-        print(response.response + "\n")
+# Create a thread and attach the file to the message
+thread = client.beta.threads.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "What is the cost of buying service?",
+            # Attach the new file to the message.
+            "attachments": [
+                {"file_id": message_file.id, "tools": [
+                    {"type": "file_search"}]}
+            ],
+        }
+    ]
+)
 
+# The thread now has a vector store with that file in its tool resources.
+print(thread.tool_resources.file_search)
 
-def main():
-    os.environ["OPENAI_API_KEY"] = input(
-        "Paste your OpenAI key here and hit enter:")
-    construct_index("context_data/data")
-    ask_ai()
+run = client.beta.threads.runs.create_and_poll(
+    thread_id=thread.id, assistant_id=assistant.id
+)
 
+messages = list(client.beta.threads.messages.list(
+    thread_id=thread.id, run_id=run.id))
 
-if __name__ == "__main__":
-    main()
+message_content = messages[0].content[0].text
+annotations = message_content.annotations
+citations = []
+for index, annotation in enumerate(annotations):
+    message_content.value = message_content.value.replace(
+        annotation.text, f"[{index}]")
+    if file_citation := getattr(annotation, "file_citation", None):
+        cited_file = client.files.retrieve(file_citation.file_id)
+        citations.append(f"[{index}] {cited_file.filename}")
+
+print(message_content.value)
+print("\n".join(citations))
